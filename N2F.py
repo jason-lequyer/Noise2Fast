4b3feecffd221b0be8078920bdca3a518f108302
pelletierlab <80357607+pelletierlab@users.noreply.github.com>
2021-05-15 11:04:34 -0400

    Add files via upload

--- N2F.py
+++ N2F.py
@@ -55,15 +55,12 @@ if __name__ == "__main__":
         
     #load data
     file_list = [f for f in os.listdir(folder)]
+    start_time = time.time()
     for v in range(len(file_list)):
-        start_time = time.time()
+        
         file_name =  file_list[v]
         print(file_name)
         
-        net = Net()
-        net.to(device)
-        criterion = nn.BCELoss()
-        optimizer = optim.Adam(net.parameters(), lr=0.001)
         
         img = imread(folder + '/' + file_name)
         
@@ -94,8 +91,16 @@ if __name__ == "__main__":
                 if j % 2 == 1:
                     imgin[i,j] = imgZ[2*i,j]
                     imgin2[i,j] = imgZ[2*i+1,j]
-        listimgH.append(imgin.copy())
-        listimgH.append(imgin2.copy())
+        imgin = torch.from_numpy(imgin)
+        imgin = torch.unsqueeze(imgin,0)
+        imgin = torch.unsqueeze(imgin,0)
+        imgin = imgin.to(device)
+        imgin2 = torch.from_numpy(imgin2)
+        imgin2 = torch.unsqueeze(imgin2,0)
+        imgin2 = torch.unsqueeze(imgin2,0)
+        imgin2 = imgin2.to(device)
+        listimgH.append(imgin)
+        listimgH.append(imgin2)
         
         listimgV = []
         Zshape = [shape[0],shape[1]]
@@ -115,8 +120,16 @@ if __name__ == "__main__":
                 if i % 2 == 1:
                     imgin3[i,j] = imgZ[i,2*j]
                     imgin4[i,j] = imgZ[i,2*j+1]
-        listimgV.append(imgin3.copy())
-        listimgV.append(imgin4.copy())
+        imgin3 = torch.from_numpy(imgin3)
+        imgin3 = torch.unsqueeze(imgin3,0)
+        imgin3 = torch.unsqueeze(imgin3,0)
+        imgin3 = imgin3.to(device)
+        imgin4 = torch.from_numpy(imgin4)
+        imgin4 = torch.unsqueeze(imgin4,0)
+        imgin4 = torch.unsqueeze(imgin4,0)
+        imgin4 = imgin4.to(device)
+        listimgV.append(imgin3)
+        listimgV.append(imgin4)
         
     
         img = torch.from_numpy(img)
@@ -130,30 +143,11 @@ if __name__ == "__main__":
         listimgH2 = [[listimgH[0],listimgH[1]]]
         listimg = listimgH1+listimgH2+listimgV1+listimgV2
         
-        class MyDataSet(utils_data.Dataset):
-          def __init__(self, transform=None):
-              self.image_pair_list = listimg
-              
-          def __len__(self):
-              return len(self.image_pair_list)
-          
-          def __getitem__(self, index):
-              image_pair =  self.image_pair_list[index]
-              
-              
-              mask = image_pair[0]
-              label = image_pair[1]
-              mask = torch.from_numpy(mask)
-              mask = torch.unsqueeze(mask,0)
-              label = torch.from_numpy(label)
-              label = torch.unsqueeze(label,0)
-              
-              
-              
-              return mask, label
+        net = Net()
+        net.to(device)
+        criterion = nn.BCELoss()
+        optimizer = optim.Adam(net.parameters(), lr=0.001)
         
-        trainset = MyDataSet()
-        trainloader = torch.utils.data.DataLoader(trainset, batch_size=1, shuffle=False, num_workers=4)
         
         
         running_loss1=0.0
@@ -164,43 +158,44 @@ if __name__ == "__main__":
         last10psnr = [0]*105
         cleaned = 0
         while timesince <= tsince:
-    
-            for i, data in enumerate(trainloader):
-                inputs, labello = data[0].to(device), data[1].to(device)
+            indx = np.random.randint(0,len(listimg))
+            data = listimg[indx]
+            inputs = data[0]
+            labello = data[1]
+            
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss1 = criterion(outputs, labello)
+            loss = loss1
+            running_loss1+=loss1.item()
+            loss.backward()
+            optimizer.step()
+            
+            
+            running_loss1=0.0
+            with torch.no_grad():
+                last10.pop(0)
+                last10.append(cleaned*maxer+minner)
+                outputstest = net(img)
+                cleaned = outputstest[0,0,:,:].cpu().detach().numpy()
                 
-                optimizer.zero_grad()
-                outputs = net(inputs)
-                loss1 = criterion(outputs, labello)
-                loss = loss1
-                running_loss1+=loss1.item()
-                loss.backward()
-                optimizer.step()
-                
-                
-                running_loss1=0.0
-                with torch.no_grad():
-                    last10.pop(0)
-                    last10.append(cleaned*maxer+minner)
-                    outputstest = net(img)
-                    cleaned = outputstest[0,0,:,:].cpu().detach().numpy()
-                    
-                    noisy = img.cpu().detach().numpy()
-                    ps = -np.mean((noisy-cleaned)**2)
-                    last10psnr.pop(0)
-                    last10psnr.append(ps)
-                    if ps > maxpsnr:
-                        maxpsnr = ps
-                        outclean = cleaned*maxer+minner
-                        timesince = 0
-                    else:
-                        timesince+=1.0
+                noisy = img.cpu().detach().numpy()
+                ps = -np.mean((noisy-cleaned)**2)
+                last10psnr.pop(0)
+                last10psnr.append(ps)
+                if ps > maxpsnr:
+                    maxpsnr = ps
+                    outclean = cleaned*maxer+minner
+                    timesince = 0
+                else:
+                    timesince+=1.0
                         
         
         H = np.mean(last10, axis=0)
         
         imwrite(outfolder + '/' + file_name, H)
         
-        print("--- %s seconds ---" % (time.time() - start_time))
         
         
         torch.cuda.empty_cache()
+    print("--- %s seconds ---" % (time.time() - start_time))

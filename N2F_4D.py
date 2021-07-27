9c68f3decd6953fc19044d3efc74eb4a6b3c08da
pelletierlab <80357607+pelletierlab@users.noreply.github.com>
2021-07-27 19:08:03 -0400

    Add files via upload

--- N2F_4D.py
+++ N2F_4D.py
@@ -62,143 +62,146 @@ if __name__ == "__main__":
         
         
         inp = imread(folder + '/' + file_name)
-        out = np.zeros(inp.shape, dtype=np.float32)
         start_time = time.time()
+        ogshape = inp.shape
+        inp = inp.reshape(-1,ogshape[-2],ogshape[-1])
+        out = np.zeros(inp.shape, dtype=np.float32)
         for oz in range(inp.shape[0]):
-            for gq in range(inp.shape[1]):
-                for sv in range(inp.shape[2]):
-                    print((oz,gq,sv))
-                    img = inp[oz,gq,sv,:,:]
-                    minner = np.amin(img)
-                    img = img - minner
-                    maxer = np.amax(img)
-                    img = img/maxer
-                    img = img.astype(np.float32)
-                    shape = img.shape
-                    
-                    
-                    
+            print('Slice '+str(oz+1)+'/'+str(inp.shape[0]))
+            img = inp[oz,:,:]
+            typer = type(inp[0,0,0])
+            minner = np.amin(img)
+            img = img - minner
+            maxer = np.amax(img)
+            img = img/maxer
+            img = img.astype(np.float32)
+            shape = img.shape
+            
+            
+            
+        
+            listimgH = []
+            Zshape = [shape[0],shape[1]]
+            if shape[0] % 2 == 1:
+                Zshape[0] -= 1
+            if shape[1] % 2 == 1:
+                Zshape[1] -=1  
+            imgZ = img[:Zshape[0],:Zshape[1]]
+            
+            imgin = np.zeros((Zshape[0]//2,Zshape[1]),dtype=np.float32)
+            imgin2 = np.zeros((Zshape[0]//2,Zshape[1]),dtype=np.float32)
+            for i in range(imgin.shape[0]):
+                for j in range(imgin.shape[1]):
+                    if j % 2 == 0:
+                        imgin[i,j] = imgZ[2*i+1,j]
+                        imgin2[i,j] = imgZ[2*i,j]
+                    if j % 2 == 1:
+                        imgin[i,j] = imgZ[2*i,j]
+                        imgin2[i,j] = imgZ[2*i+1,j]
+            imgin = torch.from_numpy(imgin)
+            imgin = torch.unsqueeze(imgin,0)
+            imgin = torch.unsqueeze(imgin,0)
+            imgin = imgin.to(device)
+            imgin2 = torch.from_numpy(imgin2)
+            imgin2 = torch.unsqueeze(imgin2,0)
+            imgin2 = torch.unsqueeze(imgin2,0)
+            imgin2 = imgin2.to(device)
+            listimgH.append(imgin)
+            listimgH.append(imgin2)
+            
+            listimgV = []
+            Zshape = [shape[0],shape[1]]
+            if shape[0] % 2 == 1:
+                Zshape[0] -= 1
+            if shape[1] % 2 == 1:
+                Zshape[1] -=1  
+            imgZ = img[:Zshape[0],:Zshape[1]]
+            
+            imgin3 = np.zeros((Zshape[0],Zshape[1]//2),dtype=np.float32)
+            imgin4 = np.zeros((Zshape[0],Zshape[1]//2),dtype=np.float32)
+            for i in range(imgin3.shape[0]):
+                for j in range(imgin3.shape[1]):
+                    if i % 2 == 0:
+                        imgin3[i,j] = imgZ[i,2*j+1]
+                        imgin4[i,j] = imgZ[i, 2*j]
+                    if i % 2 == 1:
+                        imgin3[i,j] = imgZ[i,2*j]
+                        imgin4[i,j] = imgZ[i,2*j+1]
+            imgin3 = torch.from_numpy(imgin3)
+            imgin3 = torch.unsqueeze(imgin3,0)
+            imgin3 = torch.unsqueeze(imgin3,0)
+            imgin3 = imgin3.to(device)
+            imgin4 = torch.from_numpy(imgin4)
+            imgin4 = torch.unsqueeze(imgin4,0)
+            imgin4 = torch.unsqueeze(imgin4,0)
+            imgin4 = imgin4.to(device)
+            listimgV.append(imgin3)
+            listimgV.append(imgin4)
+            
+        
+            img = torch.from_numpy(img)
+            img = torch.unsqueeze(img,0)
+            img = torch.unsqueeze(img,0)
+            img = img.to(device)
+            
+            listimgV1 = [[listimgV[0],listimgV[1]]]
+            listimgV2 = [[listimgV[1],listimgV[0]]]
+            listimgH1 = [[listimgH[1],listimgH[0]]]
+            listimgH2 = [[listimgH[0],listimgH[1]]]
+            listimg = listimgH1+listimgH2+listimgV1+listimgV2
+            
+            net = Net()
+            net.to(device)
+            criterion = nn.BCELoss()
+            optimizer = optim.Adam(net.parameters(), lr=0.001)
+            
+            
+            
+            running_loss1=0.0
+            running_loss2=0.0
+            maxpsnr = -np.inf
+            timesince = 0
+            last10 = [0]*105
+            last10psnr = [0]*105
+            cleaned = 0
+            while timesince <= tsince:
+                indx = np.random.randint(0,len(listimg))
+                data = listimg[indx]
+                inputs = data[0]
+                labello = data[1]
                 
-                    listimgH = []
-                    Zshape = [shape[0],shape[1]]
-                    if shape[0] % 2 == 1:
-                        Zshape[0] -= 1
-                    if shape[1] % 2 == 1:
-                        Zshape[1] -=1  
-                    imgZ = img[:Zshape[0],:Zshape[1]]
-                    
-                    imgin = np.zeros((Zshape[0]//2,Zshape[1]),dtype=np.float32)
-                    imgin2 = np.zeros((Zshape[0]//2,Zshape[1]),dtype=np.float32)
-                    for i in range(imgin.shape[0]):
-                        for j in range(imgin.shape[1]):
-                            if j % 2 == 0:
-                                imgin[i,j] = imgZ[2*i+1,j]
-                                imgin2[i,j] = imgZ[2*i,j]
-                            if j % 2 == 1:
-                                imgin[i,j] = imgZ[2*i,j]
-                                imgin2[i,j] = imgZ[2*i+1,j]
-                    imgin = torch.from_numpy(imgin)
-                    imgin = torch.unsqueeze(imgin,0)
-                    imgin = torch.unsqueeze(imgin,0)
-                    imgin = imgin.to(device)
-                    imgin2 = torch.from_numpy(imgin2)
-                    imgin2 = torch.unsqueeze(imgin2,0)
-                    imgin2 = torch.unsqueeze(imgin2,0)
-                    imgin2 = imgin2.to(device)
-                    listimgH.append(imgin)
-                    listimgH.append(imgin2)
-                    
-                    listimgV = []
-                    Zshape = [shape[0],shape[1]]
-                    if shape[0] % 2 == 1:
-                        Zshape[0] -= 1
-                    if shape[1] % 2 == 1:
-                        Zshape[1] -=1  
-                    imgZ = img[:Zshape[0],:Zshape[1]]
-                    
-                    imgin3 = np.zeros((Zshape[0],Zshape[1]//2),dtype=np.float32)
-                    imgin4 = np.zeros((Zshape[0],Zshape[1]//2),dtype=np.float32)
-                    for i in range(imgin3.shape[0]):
-                        for j in range(imgin3.shape[1]):
-                            if i % 2 == 0:
-                                imgin3[i,j] = imgZ[i,2*j+1]
-                                imgin4[i,j] = imgZ[i, 2*j]
-                            if i % 2 == 1:
-                                imgin3[i,j] = imgZ[i,2*j]
-                                imgin4[i,j] = imgZ[i,2*j+1]
-                    imgin3 = torch.from_numpy(imgin3)
-                    imgin3 = torch.unsqueeze(imgin3,0)
-                    imgin3 = torch.unsqueeze(imgin3,0)
-                    imgin3 = imgin3.to(device)
-                    imgin4 = torch.from_numpy(imgin4)
-                    imgin4 = torch.unsqueeze(imgin4,0)
-                    imgin4 = torch.unsqueeze(imgin4,0)
-                    imgin4 = imgin4.to(device)
-                    listimgV.append(imgin3)
-                    listimgV.append(imgin4)
-                    
+                optimizer.zero_grad()
+                outputs = net(inputs)
+                loss1 = criterion(outputs, labello)
+                loss = loss1
+                running_loss1+=loss1.item()
+                loss.backward()
+                optimizer.step()
                 
-                    img = torch.from_numpy(img)
-                    img = torch.unsqueeze(img,0)
-                    img = torch.unsqueeze(img,0)
-                    img = img.to(device)
+                
+                running_loss1=0.0
+                with torch.no_grad():
+                    last10.pop(0)
+                    last10.append(cleaned*maxer+minner)
+                    outputstest = net(img)
+                    cleaned = outputstest[0,0,:,:].cpu().detach().numpy()
                     
-                    listimgV1 = [[listimgV[0],listimgV[1]]]
-                    listimgV2 = [[listimgV[1],listimgV[0]]]
-                    listimgH1 = [[listimgH[1],listimgH[0]]]
-                    listimgH2 = [[listimgH[0],listimgH[1]]]
-                    listimg = listimgH1+listimgH2+listimgV1+listimgV2
-                    
-                    net = Net()
-                    net.to(device)
-                    criterion = nn.BCELoss()
-                    optimizer = optim.Adam(net.parameters(), lr=0.001)
-                    
-                    
-                    
-                    running_loss1=0.0
-                    running_loss2=0.0
-                    maxpsnr = -np.inf
-                    timesince = 0
-                    last10 = [0]*105
-                    last10psnr = [0]*105
-                    cleaned = 0
-                    while timesince <= tsince:
-                        indx = np.random.randint(0,len(listimg))
-                        data = listimg[indx]
-                        inputs = data[0]
-                        labello = data[1]
-                        
-                        optimizer.zero_grad()
-                        outputs = net(inputs)
-                        loss1 = criterion(outputs, labello)
-                        loss = loss1
-                        running_loss1+=loss1.item()
-                        loss.backward()
-                        optimizer.step()
-                        
-                        
-                        running_loss1=0.0
-                        with torch.no_grad():
-                            last10.pop(0)
-                            last10.append(cleaned*maxer+minner)
-                            outputstest = net(img)
-                            cleaned = outputstest[0,0,:,:].cpu().detach().numpy()
+                    noisy = img.cpu().detach().numpy()
+                    ps = -np.mean((noisy-cleaned)**2)
+                    last10psnr.pop(0)
+                    last10psnr.append(ps)
+                    if ps > maxpsnr:
+                        maxpsnr = ps
+                        outclean = cleaned*maxer+minner
+                        timesince = 0
+                    else:
+                        timesince+=1.0
                             
-                            noisy = img.cpu().detach().numpy()
-                            ps = -np.mean((noisy-cleaned)**2)
-                            last10psnr.pop(0)
-                            last10psnr.append(ps)
-                            if ps > maxpsnr:
-                                maxpsnr = ps
-                                outclean = cleaned*maxer+minner
-                                timesince = 0
-                            else:
-                                timesince+=1.0
-                                    
-                    
-                    H = np.mean(last10, axis=0)
-                    out[oz,gq,sv,:,:] = H.copy()
-                    torch.cuda.empty_cache()  
-        print("--- %s seconds ---" % (time.time() - start_time))
-        imwrite(outfolder + '/' + file_name, out, imagej=True)
+            
+            H = np.mean(last10, axis=0)
+            out[oz,:,:] = H.copy()
+            torch.cuda.empty_cache()
+            print("--- %s seconds ---" % (time.time() - start_time))
+            start_time = time.time()
+        out = out.reshape(ogshape)
+        imwrite(outfolder + '/' + file_name, out.astype(typer), imagej=True)
